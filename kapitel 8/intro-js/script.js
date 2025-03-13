9ae431e90b6d96818fd68dc0deb4fdfb9882427a
Zulfiqar Mughul <zulfiqar.mughul@elev.ga.ntig.se>
2025-03-13 14:26:23 +0100

    tränat på javascript, variabler, konkatelering

---
+++ kapitel 8/intro-js/script.js
@@ -0,0 +1,17 @@
+// var tal1 = 12;
+// var tal2 = 34;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "Zen";
+var efternamn = "M";
+var ålder = 50;
+var namn = förnamn + "  " +efternamn;
+var meddelande = namn + " är " + ålder + " år gammal. ";
+// alert(meddelande);
+
+
+var hobby = "spela"
+var pris = 0
+var meddelande = namn + " gillar att " + hobby + " \noch det kostar " + pris +"kr" + " per år ";
+alert(meddelande);
