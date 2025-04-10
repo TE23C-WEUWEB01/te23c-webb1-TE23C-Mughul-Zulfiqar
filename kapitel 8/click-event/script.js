a7965124df530cc76b6c1a5f74b333f52926485a
Zulfiqar Mughul <zulfiqar.mughul@elev.ga.ntig.se>
2025-04-10 14:20:47 +0200

    tränat på click-event,

---
+++ kapitel 8/click-event/script.js
@@ -0,0 +1,30 @@
+function setDarkMode() {
+    document.body.style = "background: #000;color: #fff;";
+}
+function setLightMode() {
+    document.body.style = "background: #fff;color: #000";
+}
+function setFunnyMode() {
+    document.querySelector("h1").style = "font-size:100px;";   
+}
+function setNormalMode() {
+    document.querySelector("h1").style = "font-size:32px;";   
+}
+function setFunnyMode() {
+    document.querySelector("h1").style = "color: #fbc;font-size:100px"; 
+}
+function Hide() {
+    document.querySelector("h1").style = "visibility: hidden;";
+}
+function Show() {
+    document.querySelector("h1").style = "visibility: visible;";
+}
+function Varning() {
+    document.querySelector("p").style = "visibility: visible;";
+}
+function Göm() {
+    document.querySelector("p").style = "visibility: hidden;";
+}
+function visaBild() {
+    document.querySelector("img").style = "visibility: visible;";
+}
